cfd555f8b4a6f61d3b28dbc3a53aa56a94931308
Saurabh negi <negis9705@gmail.com>
2024-12-22 09:35:35 +0530

    add sendVerificationEmail function - it will send otp via email

--- src/helper/sendVerificationEmail.ts
+++ src/helper/sendVerificationEmail.ts
@@ -1,2 +1,26 @@
 import  {resend} from "../lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
+import {ApiResponse} from "@/types/apiResponse"
+
+
+
+export async function sendVerificationEmail(email:string, username: string, verifyCode: string
+    ): Promise<ApiResponse> {
+    try {
+        await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject:'Mystry Message | verifyCode',
+            react:VerificationEmail({username, otp:verifyCode}),
+          });
+      
+          return {
+            success: true, message: 'verification email send successfullly'
+        }
+    } catch (emailError) {
+        console.error("Error sending verification email", emailError)
+        return { 
+            success: false, message: 'Failed to send verification email'
+        }
+    }
+}
--- src/types/apiResponse.ts
+++ src/types/apiResponse.ts
@@ -1,10 +1,9 @@
 import { boolean } from "zod";
 import {Message} from "../model/User" 
   
-export interface ApiResponse<T> {
+export interface ApiResponse{
     success: boolean;
     message: string;
     isAcceptingMessages?: boolean
     messages?: Array<Message>
-    data?: T; // Generic type to allow flexibility
 }

eb932b94cc156989637b37887074f534ce27c5d5
Saurabh negi <negis9705@gmail.com>
2024-12-26 11:19:03 +0530

    add middleware , auth.route

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -68,7 +68,7 @@ export const authOptions: NextAuthOptions = {
          session.user._id = token._id;
          session.user.isVerified = token.isVerified;
          session.user.isAcceptingMessage = token.isAcceptingMessage;
-         session.user.usernames = token.usernames;
+         session.user.username = token.username;
       }
       return session
     },
